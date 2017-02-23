8ed189d5dc5cf6a482032b2ad5c8770acbe2ea96
Doug <djeremias@getabby.com>
2017-02-23 13:46:36 -0500

    starting on angular front end stuff, and added dockerFile to install phase of build

--- ab-web/src/main/app/app-controller.js
+++ ab-web/src/main/app/app-controller.js
@@ -1,3 +1,17 @@
 /**
  * Created by djeremias on 1/13/17.
  */
+abApp.controller('AppController', [ '$scope', '$rootScope', '$state', '$locale','LoginService',
+    function($scope, $rootScope, $state, $locale, LoginService) {
+
+        $scope.doLogout = function (){
+            LoginService.logout().then(function(respose){
+                $state.go('login', {});
+            });
+        };
+
+        $scope.isLoggedIn = function(){
+            return LoginService.isLoggedIn();
+        };
+    }
+]);

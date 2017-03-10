fcf1158b2eb22b2f0d8f81cea3a82b89e28d2d31
Doug <djeremias@getabby.com>
2017-03-10 16:34:35 -0500

    Moved to spring boot 1.5.2 and some other clean up.

--- ab-rest/src/main/java/io/doug/RestApplication.java
+++ ab-rest/src/main/java/io/doug/RestApplication.java
@@ -1,17 +1,12 @@
 package io.doug;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 /**
  * Created by djeremias on 1/3/17.
  */
 @Import(DomainApplication.class)
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class
-})
 public class RestApplication {
 
     public static void main(String[] args) {
